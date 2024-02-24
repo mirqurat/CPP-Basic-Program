cf48cacb8139000117df4ac7a987aaac0d594cf5
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-02-24 15:43:10 +0530

    Add files via upload

---
+++ all programs here/while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=1,a;
+	//cout<<"print hello on enter any key"<<endl;
+	//cin>>a;
+	do
+	{
+		cout<<"hello" <<endl;
+		i++;
+	}	
+		while(i<3);
+return 0;	
+}
