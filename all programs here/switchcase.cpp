77649f80e8b835e7eaf5d433a4668593f9a90c57
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-02-24 15:54:17 +0530

    use of switch case

---
+++ all programs here/switchcase.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a;
+cout<<"enter any number \n";
+cin>>a;
+switch(a){
+		case 1: 
+	cout<<"hello";
+	break;
+		case 2: 
+	cout<<"asalamualikum";
+	break;
+		case 3: 
+	cout<<"namasta";
+	break;
+		case 4: 
+	cout<<"good morinig";
+	break;
+		case 5: 
+	cout<<"hola";
+	break;
+		case 6: 
+	cout<<"german";
+	break;
+		case 7: 
+	cout<<"salve";
+	break;
+		default:
+	cout<<"nothing available";
+	
+}
+}
